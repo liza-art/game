c5356c187e2dbb75e52cdbedde0379a4720618ff
liza-art <lizagvozd70@gmail.com>
2024-06-05 12:01:46 +0300

    deleted an extra fragment

--- script.js
+++ script.js
@@ -1,63 +1,62 @@
-function playGame(){
-   let humanScore = 0;
-   let computerScore = 0;
-
-     for(let i = 0; i < 5; i ++){
-      let computerChoise = getComputerChoice();
-      let humanChoise =  prompt('Input rock, scissors or paper');
+const buttons = document.querySelectorAll('button');
+const resultDiv = document.getElementById('results');
+const winnerDiv = document.getElementById('winner');
+const humanSpanScore = document.getElementsByClassName('humanSpan');
+const computerSpanScore = document.getElementsByClassName('computerSpan');
 
 
-      const winner = playRound(humanChoise, computerChoise);
-
-      if(winner === humanChoise){
-        humanScore++;
-        console.log('You win');
-      }else if('You lose'){
-        computerScore++;
-      }else{
-        console.log('its a tie');
-      }
-     };
-
-     console.log(`Your score - ${humanScore}, computer Score - ${computerScore}`);
-
-     if(humanScore > computerScore){
-      console.log('you winer!');
-     }else if(humanScore < computerScore){
-      console.log('you lose the game');
-     }else {
-      console.log('its a tie');
-     }
-  };
+let humanScore =0;
+let computerScore =0;
 
 
-  function getComputerChoice(){
-    let computerMove = '';
-     let randomN = Math.random();
-     if(randomN < 1/3){
-       computerMove = 'rock';
-     } else if(randomN < 2/3){
-      computerMove = 'scissors';
-     }else{
-      computerMove = 'paper';
-     }
-
-     return computerMove;
-   };
-
-
-  function playRound(humanChoise, computerChoise){
-
-    if(humanChoise === computerChoise){
-      return null;
-    } else if(
-      (humanChoise === 'rock' && computerChoise === 'scissors') ||
-      (humanChoise === 'scissors' && computerChoise === 'paper') ||
-      (humanChoise === 'paper' && computerChoise === 'rock')){
-        return humanChoise;
-      }else{
-        return computerChoise;
-      }
+function getComputerChoice() {
+  let computerMove = "";
+  let randomN = Math.random();
+  if (randomN < 1 / 3) {
+    computerMove = "rock";
+  } else if (randomN < 2 / 3) {
+    computerMove = "scissors";
+  } else {
+    computerMove = "paper";
   }
 
-  playGame();
+  return computerMove;
+}
+
+function playRound(humanChoise, computerChoise) {
+  if (humanChoise === computerChoise) {
+    resultDiv.textContent = 'Its a tie!!!'
+  } else if (
+    (humanChoise === "rock" && computerChoise === "scissors") ||
+    (humanChoise === "scissors" && computerChoise === "paper") ||
+    (humanChoise === "paper" && computerChoise === "rock")
+  ) {
+    resultDiv.textContent = 'You win! ;)';
+    humanScore++;
+  } else {
+    resultDiv.textContent = 'You lose. (((';
+    computerScore++;
+  }
+
+  humanSpanScore.textContent = humanScore;
+  computerSpanScore.textContent = computerScore;
+
+}
+
+function defineWinner (){
+    const winnerText = humanScore > computerScore ?
+    'You winner today. Your score ' + humanScore 
+    : humanScore === computerScore 
+    ? 'Tie..... ' 
+   :  'You lose today. Computer score ' + computerScore;
+    return winnerDiv.textContent = winnerText;
+
+}
+
+buttons.forEach((button) => addEventListener('click', function(){
+  const humanChoise= this.id;
+  const computerChoise = getComputerChoice();
+  playRound(humanChoise, computerChoise);
+}));
+
+
